3885fdb79da867591506c345d08aa4f852b0fc16
Beth Fraser <bethanyfraser@live.co.uk>
2016-01-27 22:19:19 +0000

    added protection spell test and functionality

--- adventure_game.js
+++ adventure_game.js
@@ -1,14 +1,21 @@
+Array.prototype.contains = function ( needle ) {
+   for (i in this) {
+       if (this[i] == needle) return true;
+   }
+   return false;
+}
+
+
 var Hero = function(name, health, faveFood){
   this.name = name;
   this.health = health;
   this.faveFood = faveFood;
-
   this.stomach = [];
   this.antidote = 0;
+  this.spells = [];
 }
 
 Hero.prototype = {
-
   talk: function(){
     return "I am a hero called " + this.name;
   },
@@ -37,13 +44,13 @@ Hero.prototype = {
       }}
     },
     hit: function(victim, damage){
+      if (!victim.spells.contains("protection")){
       victim.health -= damage;
+      }
     }
 
   }
 
-  var hero1 = new Hero('Callum', 100, 'beer');
-  var villain = new Hero('Beth', 100, 'pizza');
 
   var Food = function(name, poisoned, repValue){
     this.name = name;
@@ -61,9 +68,12 @@ Hero.prototype = {
     }
   }
 
-  var rat1 = new Rat("ratty");
 
-  var food1 = new Food('bread', false, 20);
-  var food2 = new Food('beer', false, 40);
+  // var hero1 = new Hero('Callum', 100, 'beer');
+  // var villain = new Hero('Beth', 100, 'pizza');
+  // var rat1 = new Rat("ratty");
+  // var food1 = new Food('bread', false, 20);
+  // var food2 = new Food('beer', false, 40);
+
 
   module.exports = [Hero, Food, Rat]
--- adventure_game_testing.js
+++ adventure_game_testing.js
@@ -9,26 +9,26 @@ var Rat = require('./adventure_game')[2];
 
 
 describe('hero', function(){
-  it('can speak!!!', function(){
+  it('can speak their name', function(){
     var hero = new Hero('Callum', 100, 'beer');
     expect(hero.talk()).equal("I am a hero called Callum");
   });
 
-  it('can eat food', function(){
+  it('can eat food, which goes into their stomach', function(){
     var hero = new Hero('Callum', 100, 'beer');
     var food = new Food('bread', false, 20);
     hero.eat(food);
     expect(hero.stomach).to.include(food);
   });
 
-  it('can eat food to heal self by food.value', function(){
+  it("can eat food to heal self by food's value", function(){
     var hero = new Hero('Callum', 100, 'beer');
     var food = new Food('bread', false, 20);
     hero.eat(food);
     expect(hero.health).to.equal(120);
   });
 
-  it('should receive 1.5 times health points from fave food', function(){
+  it('should receive 1.5 times health points from favourite food', function(){
     var hero = new Hero('Callum', 100, 'beer');
     var faveFood = new Food('beer', false, 20);
     hero.eat(faveFood);
@@ -47,22 +47,33 @@ describe('hero', function(){
     var villain = new Hero('Beth', 100, 'pizza');
     villain.hit(hero, 10);
     expect(hero.health).to.equal(90);
-  })
+  });
+
   it('can avoid being poisoned by taking an antidote', function(){
     var hero = new Hero('Callum', 100, 'beer');
     hero.antidote = 1;
     var badFood = new Food('bread', true, 20);
     hero.eat(badFood);
     expect(hero.health).to.equal(120);
-  })
+  });
 
-})
+  it('can have a protection spell which prevents damage from hits', function(){
+    var hero = new Hero('Beth', 100, 'pizza');
+    var villain = new Hero('Joker', 100, 'cereal');
+    hero.spells.push("protection");
+    villain.hit(hero, 20);
+    expect(hero.health).to.equal(100);
+
+  });
+
+});
+
 
 describe('rat', function(){
-  it('can poison food', function(){
+  it('can poison food by touching it', function(){
     var rat = new Rat('Scabbers');
     var food = new Food('bread', false, 20);
     rat.touch(food);
     expect(food.poisoned).to.equal(true);
-  })
-})
+  });
+});
